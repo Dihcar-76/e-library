cd3172d820846999f9ca73a9b4664919336d2e22
Dihcar-76 <35745701+Dihcar-76@users.noreply.github.com>
2019-06-17 11:06:12 +0200

    delete namedqueries from accountDaoBdd

--- src/main/java/org/rboug/application/elibrary/dao/AccountDaoBdd.java
+++ src/main/java/org/rboug/application/elibrary/dao/AccountDaoBdd.java
@@ -4,29 +4,15 @@ import org.rboug.application.elibrary.model.User;
 
 import javax.persistence.*;
 
-@NamedQueries({
-        @NamedQuery(name = User.FIND_BY_EMAIL, query = "SELECT u FROM User u WHERE u.email = :email"),
-        @NamedQuery(name = User.FIND_BY_UUID, query = "SELECT u FROM User u WHERE u.uuid = :uuid"),
-        @NamedQuery(name = User.FIND_BY_LOGIN, query = "SELECT u FROM User u WHERE u.login = :login"),
-        @NamedQuery(name = AccountDaoBdd.FIND_BY_LOGIN_PASSWORD, query = "SELECT u FROM User u WHERE u.login = :login AND u.password = :password"),
-        @NamedQuery(name = User.FIND_ALL, query = "SELECT u FROM User u"),
-        @NamedQuery(name = User.UPDATE_UUID, query = "UPDATE User u SET u.uuid = null WHERE u.login = :login")
-})
-public class AccountDaoBdd implements  AccountDaoBddInterface{
 
-    public static final String FIND_BY_EMAIL = "User.findByEmail";
-    public static final String FIND_BY_LOGIN = "User.findByLogin";
-    public static final String FIND_BY_UUID = "User.findByUUID";
-    public static final String FIND_BY_LOGIN_PASSWORD = "User.findByLoginAndPassword";
-    public static final String FIND_ALL = "User.findAll";
-    public static final String UPDATE_UUID = "User.update_UUID";
+public class AccountDaoBdd implements  AccountDaoBddInterface{
 
     @PersistenceContext(unitName = "elibraryPU", type = PersistenceContextType.EXTENDED)
     private EntityManager entityManager;
 
 
     public boolean userExist(String login) {
-        return entityManager.createNamedQuery(AccountDaoBdd.FIND_BY_LOGIN, User.class).setParameter("login", login)
+        return entityManager.createNamedQuery(User.FIND_BY_LOGIN, User.class).setParameter("login", login)
                 .getResultList().size() > 0;
     }
 
@@ -40,7 +26,7 @@ public class AccountDaoBdd implements  AccountDaoBddInterface{
     }
 
     public User findByLoginAndPassword(String login, String digestPassword){
-        TypedQuery<User> query = entityManager.createNamedQuery(AccountDaoBdd.FIND_BY_LOGIN_PASSWORD, User.class);
+        TypedQuery<User> query = entityManager.createNamedQuery(User.FIND_BY_LOGIN_PASSWORD, User.class);
         query.setParameter("login", login);
         query.setParameter("password", digestPassword);
         try{
