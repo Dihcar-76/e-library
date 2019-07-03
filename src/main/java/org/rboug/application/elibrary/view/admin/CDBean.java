e999eec755e883a53ccb64fc349a3d3c6dd03d1c
Dihcar-76 <35745701+Dihcar-76@users.noreply.github.com>
2019-07-03 13:33:27 +0200

    put image in cd

--- src/main/java/org/rboug/application/elibrary/view/admin/CDBean.java
+++ src/main/java/org/rboug/application/elibrary/view/admin/CDBean.java
@@ -1,6 +1,7 @@
 package org.rboug.application.elibrary.view.admin;
 
 
+import org.primefaces.model.UploadedFile;
 import org.rboug.application.elibrary.model.CD;
 import org.rboug.application.elibrary.model.Genre;
 import org.rboug.application.elibrary.model.Label;
@@ -25,9 +26,13 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
 
@@ -63,14 +68,12 @@ public class CDBean implements Serializable {
     @Resource
     private SessionContext sessionContext;
     private CD add = new CD();
+    private UploadedFile file;
 
     public Long getId() {
         return this.id;
     }
 
-   /*
-    * Support updating and deleting CD entities
-    */
 
     public void setId(Long id) {
         this.id = id;
@@ -80,14 +83,85 @@ public class CDBean implements Serializable {
         return this.CD;
     }
 
-   /*
-    * Support searching CD entities with pagination
-    */
-
     public void setCD(CD CD) {
         this.CD = CD;
     }
 
+    public int getPage() {
+        return this.page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return 10;
+    }
+
+    public CD getExample() {
+        return this.example;
+    }
+
+    public void setExample(CD example) {
+        this.example = example;
+    }
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    // ======================================
+    // =        methods         =
+    // ======================================
+
+    public void uploadImage() {//FileUploadEvent  event
+        if (Objects.nonNull(this.file)) {
+            try {
+                //this.file = event.getFile();
+                InputStream input = this.file.getInputstream();
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                for (int length = 0; (length = input.read(buffer)) > 0; ) {
+                    output.write(buffer, 0, length);
+                }
+                this.CD.setSmallImage(output.toByteArray());
+                //entityManager.merge(book);
+                FacesMessage message = new FacesMessage("Uploaded!" + file.getFileName());
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            } catch (IOException e) {
+                FacesMessage message = new FacesMessage("Error upload." + file.getFileName());
+                FacesContext.getCurrentInstance().addMessage(null, message);
+                e.printStackTrace();
+            }
+        } else {
+            FacesMessage message = new FacesMessage("Image is empty.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            if (Objects.isNull(CD.getSmallImage())) {
+                try {
+                    InputStream input = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/resources/noimage.png");
+                    ByteArrayOutputStream output = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    for (int length = 0; (length = input.read(buffer)) > 0; ) {
+                        output.write(buffer, 0, length);
+                    }
+                    this.CD.setSmallImage(output.toByteArray());
+
+                } catch (IOException e) {
+                    FacesMessage message1 = new FacesMessage("Error upload no image file." + file.getFileName());
+                    FacesContext.getCurrentInstance().addMessage(null, message1);
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /*
+     * Support updating and deleting CD entities
+     */
     public String create() {
 
         this.conversation.begin();
@@ -120,7 +194,7 @@ public class CDBean implements Serializable {
 
     public String update() {
         this.conversation.end();
-
+        uploadImage();//image
         try {
             if (this.id == null) {
                 this.entityManager.persist(this.CD);
@@ -152,25 +226,9 @@ public class CDBean implements Serializable {
         }
     }
 
-    public int getPage() {
-        return this.page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getPageSize() {
-        return 10;
-    }
-
-    public CD getExample() {
-        return this.example;
-    }
-
-    public void setExample(CD example) {
-        this.example = example;
-    }
+    /*
+     * Support searching CD entities with pagination
+     */
 
     public String search() {
         this.page = 0;
